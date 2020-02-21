238f9a21be6db3da400fea7b340bbd8fb2b0cdc5
fuqi1001 <fuqiqi12345@gmail.com>
2020-02-21 10:57:18 -0500

    set up hot reload and db

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,8 @@ var _express = _interopRequireDefault(require("express"));
 
 var _bodyParser = _interopRequireDefault(require("body-parser"));
 
+var _database = _interopRequireDefault(require("./db/database"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
@@ -21,3 +23,4 @@ app.get('/api/hello', function (req, res) {
 app.listen(port, function () {
   return console.log("Server Started, Listening on port ".concat(port));
 });
+module.exports = app;
